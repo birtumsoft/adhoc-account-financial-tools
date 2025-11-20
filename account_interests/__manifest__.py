2dfd969f243912e80b7576c921a67f524c43957c
Irene Birtum <iic@birtum.com>
2025-11-20 12:22:11 -0300

    migration

--- account_interests/__manifest__.py
+++ account_interests/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Interests Management",
-    "version": "18.0.1.4.0",
+    "version": "19.0.1.4.0",
     "category": "Accounting",
     "sequence": 14,
     "summary": "Calculate interests for selected partners",
